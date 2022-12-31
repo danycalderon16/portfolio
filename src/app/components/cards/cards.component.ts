a64300f46106731b66ef65a588bc62b38083909c
Daniel Calderon <danycalvir@hotmail.com>
2022-12-30 20:11:16 -0700

    grid responsive

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  public projects:any = []
+
+  constructor() {
+    this.projects.push('1');
+    this.projects.push('1');
+    this.projects.push('1');
+    this.projects.push('1');
+    this.projects.push('1');
+   }
 
   ngOnInit(): void {
   }
