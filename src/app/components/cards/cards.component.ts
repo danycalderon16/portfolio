74bca97df4c2cdf668daac415db8fca7321973f0
Daniel Calderon <danycalvir@hotmail.com>
2022-12-30 23:11:00 -0700

    add data

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -14,17 +14,31 @@ export class CardsComponent implements OnInit {
     this.projects = [
       {
           "id":"1001",
-          "name":"Lista de tareas",
-          "description":"Aplicación híbrida para registrar tareas y marcarlas como completadas y descompletadas",
+          "name":"App de prestamos",
+          "description":"Aplicación de Android para realizar un seguimiento de los préstamos, tener un registro de préstamos y pagos realizados por los clientes, guardar datos importantes y generar estadísticas, con almacenamiento en la nube usando Firestore.",
           "images":["task-0"],
-          "tools":["angular","ionic"]
+          "tools":["android-studio","java","firebase"]
       },
       {
-          "id":"1001",
+          "id":"1002",
           "name":"Lista de tareas",
           "description":"Aplicación híbrida para registrar tareas y marcarlas como completadas y descompletadas",
           "images":["task-0"],
-          "tools":["angular","ionic"]
+          "tools":["angular","ionic","firebase"]
+      },
+      {
+          "id":"1003",
+          "name":"Carrito de compras",
+          "description":"Aplicación híbrida de un carrito de compras, donde se agregan productos y se simula una compra.",
+          "images":["task-0"],
+          "tools":["angular","ionic","firebase"]
+      },
+      {
+          "id":"1003",
+          "name":"Backend de página escuela segura",
+          "description":"Backend de proyecto escolar, desarrollado con Node.JS y amlacenamiento en una base de datos en PostgreSQL.",
+          "images":["task-0"],
+          "tools":["javascript","nodejs","postgresql"]
       }
   ]
    }
